f9a839fd95d5cc6ab4fbc9d804e631b43437fd4a
Diego Carlino <diego.carlino@welld.ch>
2021-12-06 09:54:53 +0100

    06

--- 06/src/main/kotlin/Main.kt
+++ 06/src/main/kotlin/Main.kt
@@ -1,7 +1,36 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.io.File
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+const val SIM_DURATION = 256
+
+fun task2(input: Collection<Int>) {
+    val result = (0 until SIM_DURATION)
+        .fold(
+            listOf(0, 1, 2, 3, 4, 5, 6, 7, 8)
+                .associateWith { 0L } +
+                    input
+                        .groupingBy { it }
+                        .eachCount()
+                        .mapValues { it.value.toLong() }
+        ) { acc, _ ->
+            acc.entries.associate { (key, _) ->
+                when (key) {
+                    8 -> 8 to (acc[0] ?: 0L)
+                    6 -> 6 to (acc[0] ?: 0L) + (acc[key + 1] ?: 0L)
+                    else -> key to (acc[key + 1] ?: 0L)
+                }
+            }
+        }
+        .values
+        .sum()
+
+    println(result)
+}
+
+fun main(args: Array<String>) {
+    val input = File("/Users/lino/dev/advent-of-code-21/06/src/main/resources/1.txt")
+        .readLines()[0]
+        .split(',')
+        .map(String::toInt)
+
+    task2(input)
 }
