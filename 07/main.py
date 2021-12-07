b43381d0a47b65dc6a491fdcbcfb872d2698efc6
Diego Carlino <diego.carlino@welld.ch>
2021-12-07 08:34:39 +0100

    07

--- 07/main.py
+++ 07/main.py
@@ -1,17 +1,28 @@
-# coding=utf-8
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from functools import reduce
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print("Hi, {0}".format(name))  # Press ⌘F8 to toggle the breakpoint.
+def task_1(input_data):
+    sorted_data = sorted(data)
+
+    target_position = sorted_data[len(input_data) / 2]
+
+    result = reduce(lambda acc, curr: acc + abs(target_position - curr), sorted_data, 0)
+    print(result)
 
 
-# Press the green button in the gutter to run the script.
+def task_2(input_data):
+    def fuel(a, b):
+        start, end = sorted([a, b])
+        return reduce(lambda acc, curr: acc + curr, [y for y in range(1, end-start+1)], 0)
+
+    target_position = reduce(lambda acc, curr: acc + curr, input_data, 1) / len(input_data)
+
+    result = reduce(lambda acc, curr: acc + fuel(target_position, curr), input_data, 0)
+    print(result)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    data = [int(x) for x in open("./1.txt").read().split(',')]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    task_1(data)
+    task_2(data)
